15cd58bfcc5ac383339112c18c5a029f3ccda600
oscar08850 <49766360+oscar08850@users.noreply.github.com>
2022-12-16 11:25:31 +0100

    Update app.module.ts
    Eliminado import repetido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,6 @@ import { TicketComponent } from './ticket/ticket.component';
 import { FormsModule } from '@angular/forms';
 import { AboutComponent } from './about/about.component';
 import { PerfilComponent } from './perfil/perfil.component';
-import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
