6d0505f4d8a2a4abd9017f3be87cc171a2077cdf
oscar08850 <oscar08850@gmail.com>
2022-12-16 09:34:33 +0100

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../models/User';
 import { LoginService } from '../service/login.service';
 
 @Component({
@@ -10,16 +13,37 @@ export class LoginComponent {
   email: String = '';
   pass: String = '';
 
+  url: String = "http://localhost:8080/users/";
+
+
   form: any = {
     email: null,
     pass: null,
   };
 
+  jsonCarrito: User[] = [];
+
+  //constructor(private loginService: LoginService) {}
+  //constructor(private http:HttpClient) {}
+  constructor(){}
+
   login(): void {
     console.log(this.email + '\n' + this.pass);
   }
 
+
+
   handleSubmit() {
-    console.log(); //Da undefined
+    console.log('LOGIN');
+
+
+    // this.loginService.login(this.form.email, this.form.password).subscribe(
+    //   (data: any) => {
+    //     //window.location.reload();
+    //   },
+    //   (error:any) => {
+    //     console.log(error);
+    //   }
+    // );
   }
 }
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+//const AUTH_URL = 'URL del railway';
 
-const AUTH_URL = 'http://localhost:8080/';
+const AUTH_URL = 'http://localhost:8080/users/';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type':'application/json'})
@@ -16,6 +17,7 @@ export class LoginService {
   constructor( private http: HttpClient) { }
 
   login(username:string, password:string): any{
+    console.log(username + " +  "  + password)
     return this.http.post(AUTH_URL + 'login', {
       username,
       password
